9f32aeeb0ebb0fdfce926912507b067c43d430cb
balani98 <balanideepanshu92@gmail.com>
2020-12-02 00:00:11 +0530

    build succeeded

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path=require('path');
 const app=express();
 app.use(express.static(__dirname+'/dist/myapp/'));
 app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/dist/angular-oauth-google/index.html'))
+    res.sendFile(path.join(__dirname+'/dist/web/index.html'))
 })
 app.listen(process.env.PORT||8080);
